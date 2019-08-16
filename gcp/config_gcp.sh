56859d5d05990143840fd872242a903cd0525bab
antoinecollas <antoine.collas@etu.utc.fr>
2019-08-16 10:23:20 +0200

    12 cores, 64gb, 4 V100

--- gcp/config_gcp.sh
+++ gcp/config_gcp.sh
@@ -1,9 +1,8 @@
 #!/bin/zsh
 PROJECT="deep-learning-dql"
 IMAGE_FAMILY="pytorch-latest-gpu"
-ZONE="europe-west4-b"
+ZONE="europe-west4-c"
 INSTANCE_NAME="server1"
-NB_CPU="12"
-MEMORY_SIZE="50"
-ACCELERATOR="type=nvidia-tesla-v100,count=1"
+MACHINE_TYPE="custom-12-65536"
+ACCELERATOR="type=nvidia-tesla-v100,count=4"
 IP_ADDRESS="35.204.106.19"
