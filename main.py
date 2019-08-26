4a60d49d7884299877f05cd1c9f3064173c3a25b
antoinecollas <antoine.collas@etu.utc.fr>
2019-08-26 11:55:24 +0200

    bug fix

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ if (training and (experiment_id is not None)) or ((not training) and (experiment
 HYPERPARAMS = {
     'pretrained': False if (debug and not torch.cuda.is_available()) else True,
     'nb_epochs': args.epoch,
-    'nb_examples': 100 if debug else float('inf'),
+    'nb_examples': 960 if debug else None,
     'scheduler': True,
     'bs': 2 if (debug and not torch.cuda.is_available()) else 48,
     'momentum': 0.9,
@@ -100,8 +100,9 @@ if training:
     df = pd.read_csv(PATH_METADATA+'/train.csv')
     df['celltype'] = df['experiment'].apply(get_celltype)
     df_train, df_val = train_test_split(df, test_size = 0.1, random_state=42)
-    df_train = df_train[:HYPERPARAMS['nb_examples']]
-    df_val = df_val[:HYPERPARAMS['nb_examples']]
+    if HYPERPARAMS['nb_examples'] is not None:
+        df_train = df_train[:HYPERPARAMS['nb_examples']]
+        df_val = df_val[:HYPERPARAMS['nb_examples']]
     print('Size training dataset: {}'.format(len(df_train)))
     print('Size validation dataset: {}'.format(len(df_val)))
 
