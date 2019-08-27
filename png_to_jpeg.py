30539626d2a9fc2c3d7bb30776ff6cd2d267d828
antoinecollas <antoine.collas@etu.utc.fr>
2019-08-27 10:38:12 +0200

    png to jpeg script

---
+++ png_to_jpeg.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os, glob
+from tqdm import tqdm
+import multiprocessing
+
+def update(*a):
+    pbar.update()
+
+def convert_png_to_jpeg(path):
+    path_jpeg = path.split('.')[0]+'.jpeg'
+    im = Image.open(path)
+    im = im.convert('L')
+    im.save(path_jpeg, quality=95)
+
+paths = glob.glob('data/**/*.png', recursive=True)
+pbar = tqdm(total=len(paths))
+pool = multiprocessing.Pool(os.cpu_count())
+for path in paths:
+    pool.apply_async(convert_png_to_jpeg, args=[path], callback=update)
+pool.close()
+pool.join()
